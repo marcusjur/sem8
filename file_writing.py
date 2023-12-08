1f9f04236eb9ab342ef3f8e9acfa0346cea5cae7
marcusjur <themaninstripesandglasses@gmail.com>
2023-12-08 21:49:11 +0300

    feat: replace op

--- file_writing.py
+++ file_writing.py
@@ -1,4 +1,4 @@
-'''
+"""
 Создать телефонный справочник с
 возможностью импорта и экспорта данных в
 формате .txt. Фамилия, имя, отчество, номер
@@ -13,28 +13,33 @@
 человека)
 4. Использование функций. Ваша программа
 не должна быть линейной
-'''
+"""
 
 from os.path import exists
 from csv import DictReader, DictWriter
+import re
+
 
 class LenNumberError(Exception):
     def __init__(self, txt):
         self.txt = txt
 
-class NameError(Exception):
+
+class InfoNameError(Exception):
     def __init__(self, txt):
         self.txt = txt
+
+
 def get_info():
     is_valid_first_name = False
     while not is_valid_first_name:
         try:
             first_name = input("Введите имя: ")
             if len(first_name) < 2:
-                raise NameError("Не валидное имя")
+                raise InfoNameError("Не валидное имя")
             else:
                 is_valid_first_name = True
-        except NameError as err:
+        except InfoNameError as err:
             print(err)
             continue
 
@@ -65,6 +70,32 @@ def create_file(file_name):
         f_writer.writeheader()
 
 
+def replace_lines(source_filename: str, destination_filename: str, line_number: int = 0):
+    """
+
+    :param source_filename:
+    :param destination_filename:
+    :param line_number:
+    :return:
+    """
+
+    try:
+        with open(source_filename, 'r', encoding='utf-8') as source_data:
+            source_lines = source_data.readlines()
+            source_line = source_lines[line_number]
+            source_data.close()
+
+        with open(destination_filename, 'w', encoding='utf-8') as destination_data:
+            destination_lines = destination_data.readlines()
+            destination_lines.append(source_line)
+            destination_data.writelines(destination_lines)
+            destination_data.close()
+    except FileNotFoundError:
+        raise FileNotFoundError('Invalid file name')
+    except UnicodeDecodeError:
+        raise ValueError('Selected file is not UTF-8 formatted')
+
+
 def read_file(file_name):
     with open(file_name, "r", encoding='utf-8') as data:
         f_reader = DictReader(data)
@@ -75,7 +106,7 @@ def write_file(file_name, lst):
     res = read_file(file_name)
     for el in res:
         if el["Телефон"] == str(lst[2]):
-            print("Такой телофон уже есть")
+            print("Такой телефон уже есть")
             return
 
     obj = {"Имя": lst[0], "Фамилия": lst[1], "Телефон": lst[2]}
@@ -103,6 +134,13 @@ def main():
                 print("Файл отсутствует. Создайте его")
                 continue
             print(*read_file(file_name))
+        elif bool(re.compile("replace", re.IGNORECASE).match(command)):
+            destination_file = input('Enter destination filename')
+            source_file = input('Enter source filename')
+            line_num = int(input('Enter line number'))
+
+            replace_lines(destination_file, source_file, line_num)
 
 
-main()
+if __name__ == "__main__":
+    main()
